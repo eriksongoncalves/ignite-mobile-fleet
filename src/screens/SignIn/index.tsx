336055c750ccd2921b7ee1c2d253923aa982844c
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2023-06-04 20:10:46 -0300

    feat: initial config

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
