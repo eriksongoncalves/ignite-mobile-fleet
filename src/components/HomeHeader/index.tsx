42e682ac48b8235ff614eb1998254f0bbc75594d
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2023-06-07 09:59:53 -0300

    feat: logout

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,25 +1,32 @@
 import { TouchableOpacity } from 'react-native'
 import { Power } from '@phosphor-icons/react'
 import { useTheme } from 'styled-components'
+import { useUser, useApp } from '@realm/react'
 
 import * as S from './styles'
 
 export default function HomeHeader() {
   const theme = useTheme()
+  const user = useUser()
+  const app = useApp()
+
+  function handleLogout() {
+    app.currentUser?.logOut()
+  }
 
   return (
     <S.Container>
       <S.Picture
-        source={{ uri: 'https://github.com/eriksongoncalves.png' }}
+        source={{ uri: user?.profile.pictureUrl }}
         placeholder="L184i9offQof00ayfQay~qj[fQj["
       />
 
       <S.Greeting>
         <S.Message>Olá</S.Message>
-        <S.Name>Erikson</S.Name>
+        <S.Name>{user?.profile.name}</S.Name>
       </S.Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </S.Container>
